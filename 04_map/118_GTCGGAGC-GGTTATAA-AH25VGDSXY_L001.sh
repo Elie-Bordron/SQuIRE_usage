7f25499c847cdf9070358109f486e4aaa8fb9381
marieBvr <lefebvre.m2@gmail.com>
2022-08-23 15:49:23 +0200

    Add alternative scripts for map step

---
+++ 04_map/118_GTCGGAGC-GGTTATAA-AH25VGDSXY_L001.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH -t 1-00:00:00
+#SBATCH --mem=15GB
+#SBATCH --cpus-per-task=8
+#SBATCH --export=ALL
+squire Map --read1 /scratch/qtbui_TE/raw_data/GEP4TWAS_rawdata/118_GTCGGAGC-GGTTATAA-AH25VGDSXY_L001_R1.fastq.gz --read2 /scratch/qtbui_TE/raw_data/GEP4TWAS_rawdata/118_GTCGGAGC-GGTTATAA-AH25VGDSXY_L001_R2.fastq.gz --map_folder /scratch/qtbui_TE/04_map/out/118_GTCGGAGC-GGTTATAA-AH25VGDSXY_L001  --read_length 100 --fetch_folder /scratch/qtbui_TE/03_fetch/Marouch --pthreads 12 --name 118_GTCGGAGC-GGTTATAA-AH25VGDSXY_L001
---
+++ 04_map/KZ74-5_AGTTCAGG-CCAACAGA-AH25VGDSXY_L001.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH -t 1-00:00:00
+#SBATCH --mem=15GB
+#SBATCH --cpus-per-task=8
+#SBATCH --export=ALL
+squire Map --read1 /scratch/qtbui_TE/raw_data/GEP4TWAS_rawdata/KZ74-5_AGTTCAGG-CCAACAGA-AH25VGDSXY_L001_R1.fastq.gz --read2 /scratch/qtbui_TE/raw_data/GEP4TWAS_rawdata/KZ74-5_AGTTCAGG-CCAACAGA-AH25VGDSXY_L001_R2.fastq.gz --map_folder /scratch/qtbui_TE/04_map/out/KZ74-5_AGTTCAGG-CCAACAGA-AH25VGDSXY_L001  --read_length 100 --fetch_folder /scratch/qtbui_TE/03_fetch/Marouch --pthreads 12 --name KZ74-5_AGTTCAGG-CCAACAGA-AH25VGDSXY_L001
